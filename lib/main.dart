d76c76c02301c0b5b14857675db50a42c4498b0c
Filip Šmehyl <filip.smehyl20@gmail.com>
2024-07-17 20:41:40 +0200

    trying to show in UI names of the forms

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:xml/xml.dart';
 
 void main() {
   runApp(const MyApp());
-  
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'M.A.S.S.',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'M.A.S.S.'),
@@ -42,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -58,24 +32,70 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> _fileNames = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadFileNames();
+  }
+
+  void _loadFileNames() {
+    // List of files manually specified (since we cannot list files directly in assets)
+    List<String> filePaths = [
+      'assets/xml_forms/form_one.xml',
+      'assets/xml_forms/form_two.xml',
+      'assets/xml_forms/form_three.xml',
+    ];
+
+    // Extract the file names from the paths and remove the .xml extension
+    setState(() {
+      _fileNames = filePaths.map((path) {
+        String fileName = path.split('/').last;
+        return fileName.replaceAll('.xml', ' ');
+      }).toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        
         backgroundColor: Color.fromARGB(255, 0, 136, 255),
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title, style: TextStyle(color: Colors.white),
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.white),
+        ),
       ),
-     )
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to M.A.S.S.',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'XML Files:',
+              style: TextStyle(
+                fontSize: 16,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            for (var fileName in _fileNames)
+              Text(
+                fileName,
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+          ],
+        ),
+      ),
     );
   }
 }
